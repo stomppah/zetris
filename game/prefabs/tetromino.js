c44a8783080c8366dc283a4135969cf038d4925c
Anthony Woodward <a.woodward6237@student.leedsmet.ac.uk>
2014-06-20 15:47:27 +0100

    tidied up initial coordinates of each tetrimino

--- game/prefabs/tetromino.js
+++ game/prefabs/tetromino.js
@@ -20,65 +20,100 @@ Tetromino.prototype.update = function () {
 Tetromino.prototype.get = function(type){
 
     /*
-     holds the coordinates of the individual blocks that make up the tetromino
-
-     'I': baseCoords = [0][0,0],
-                       [1][1,0],
-                       [2][2,0],
-                       [3][3,0];
-
-     'T': baseCoords = [0][0,0],
-                       [1][1,0],
-                       [2][0,1],
-                       [3][1,1];
-
-     'O': baseCoords = [0][0,0],
-                       [1][1,0],
-                       [2][0,1],
-                       [3][1,1];
-
-     'S': baseCoords = [0][0,0],
-                       [1][1,0],
-                       [2][1,1],
-                       [3][1,2];
-
-     'Z': baseCoords = [0][1,0],
-                       [1][1,1],
-                       [2][0,1],
-                       [3][0,2];
-
-     'J': baseCoords = [0][0,1],
-                       [1][1,1],
-                       [2][1,0],
-                       [3][2,0];
-
-     'L': baseCoords = [0][0,0],
-                       [1][1,0],
-                       [2][2,0],
-                       [3][2,1];
+     Holds the coordinates of the individual blocks that make up the tetromino.
      */
-    var baseCoords = [];
+    var baseCoords;
 
-
-
-    // generate the base coords according to the type passed
+    /*
+     Return the base coords according to the type passed
+      */
     switch (type) {
         case 'I':
-
+            /*
+                    0, 1, 2, 3
+             */
+            baseCoords = [
+                [ [0], [0,0] ],
+                [ [1], [1,0] ],
+                [ [2], [2,0] ],
+                [ [3], [3,0] ]
+            ];
             break;
         case 'T':
+            /*
+                       0,
+                    1, 2, 3
+             */
+            baseCoords = [
+                [ [0], [1,0] ],
+                [ [1], [0,1] ],
+                [ [2], [1,1] ],
+                [ [3], [2,1] ]
+            ];
             break;
         case 'O':
+            /*
+                    0, 1,
+                    2, 3,
+             */
+            baseCoords = [
+                [ [0], [0,0] ],
+                [ [1], [1,0] ],
+                [ [2], [0,1] ],
+                [ [3], [1,1] ]
+            ];
             break;
         case 'S':
+            /*
+                       0, 1,
+                    2, 3
+             */
+            baseCoords = [
+                [ [0], [1,0] ],
+                [ [1], [2,0] ],
+                [ [2], [0,1] ],
+                [ [3], [1,1] ]
+            ];
             break;
         case 'Z':
+            /*
+                    0, 1,
+                       2, 3
+             */
+            baseCoords = [
+                [ [0], [0,0] ],
+                [ [1], [1,0] ],
+                [ [2], [1,1] ],
+                [ [3], [2,1] ]
+            ];
             break;
         case 'J':
+            /*
+                    0, 1, 2,
+                          3
+             */
+            baseCoords = [
+                [ [0], [0,0] ],
+                [ [1], [1,0] ],
+                [ [2], [2,0] ],
+                [ [3], [2,1] ]
+            ];
             break;
         case 'L':
+            /*
+                    0, 1, 2,
+                    3
+             */
+            baseCoords = [
+                [ [0], [0,0] ],
+                [ [1], [1,0] ],
+                [ [2], [2,0] ],
+                [ [3], [0,1] ]
+            ]
             break;
     }
+
+    return baseCoords;
 };
 
 module.exports = Tetromino;
